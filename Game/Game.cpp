73113d8c2fb4a067081748702cef85056b9e13c0
Alex <lexa35695@gmail.com>
2020-05-13 18:12:46 +0300

    Траблы с гитом

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -49,92 +49,79 @@ void Game::newGame(int level) {
 //  }
 //}
 
-void Game::draw(RenderTarget& target, RenderStates states) const {
-    states.transform *= getTransform();
-    Color color = Color(200, 100, 200);
+void Game::draw(RenderTarget& target, RenderStates states) const{
+	states.transform *= getTransform();
+	Color color = Color(127, 127, 127);
 
-    // Рисуем меню игрового поля
-    RectangleShape shape(Vector2f(pole->GetWidth() * Cell::size + 20.f, 40.f));
-    shape.setOutlineThickness(2.f);
-    shape.setOutlineColor(color);
-    shape.setFillColor(Color::Transparent);
-    target.draw(shape, states);
+	// Рисуем меню игрового поля
+	RectangleShape shape(Vector2f(pole->GetWidth() * Cell::size + 20.f, 40.f));
+	shape.setOutlineThickness(2.f);
+	shape.setOutlineColor(color);
+	shape.setFillColor(Color::Transparent);
+	target.draw(shape, states);
 
-    // Рисуем рамку игрового поля
-    shape.setSize(Vector2f(pole->GetWidth() * Cell::size + 20.f, pole->GetHeight() * Cell::size + 60.f));
-    shape.setOutlineThickness(2.f);
-    shape.setOutlineColor(color);
-    shape.setFillColor(Color::Transparent);
-    target.draw(shape, states);
+	// Рисуем рамку игрового поля
+	shape.setSize(Vector2f(pole->GetWidth() * Cell::size + 20.f, pole->GetHeight() * Cell::size + 60.f));
+	shape.setOutlineThickness(2.f);
+	shape.setOutlineColor(color);
+	shape.setFillColor(Color::Transparent);
+	target.draw(shape, states);
 
-    //// Подготавливаем рамку для отрисовки всех клеточек
-    //shape.setSize(Vector2f(Cell::size - 2, Cell::size - 2));
-    //shape.setOutlineThickness(2.f);
-    //shape.setOutlineColor(color);
-    //shape.setFillColor(Color::Transparent);
+	//// Подготавливаем рамку для отрисовки всех клеточек
+	//shape.setSize(Vector2f(Cell::size - 2, Cell::size - 2));
+	//shape.setOutlineThickness(2.f);
+	//shape.setOutlineColor(color);
+	//shape.setFillColor(Color::Transparent);
 
-    // Подготавливаем текстовую заготовку для отрисовки чисел в клеточках
+	// Подготавливаем текстовую заготовку для отрисовки чисел в клеточках
+	
+	Text text("", font, 18);
+	text.setStyle(Text::Bold);
 
-    Text text("", font, 18);
-    text.setStyle(Text::Bold);
+	Texture Texture;
+	Texture.loadFromFile("Images/Cell.png");
+	Sprite Cell;
+	Cell.setTexture(Texture);
 
-    Texture NotOpenCellTexture;
-    Sprite Cell;
+	for (unsigned int i = 0; i < pole->GetHeight(); i++){
+		for (unsigned int j = 0; j < pole->GetWidth(); j++){
+			text.setString("");
+			if (pole->GetCellStatus(i, j) == 0){// Не открытая клеточка
+				Cell.setTextureRect(sf::IntRect(0, 0, 25, 25));
+				text.setFillColor(color);
+				int count = pole->GetCellNumber(i, j);
+				if (count == 9) {
+					text.setFillColor(Color::Black);
+					text.setString(std::to_string(count));
+				}
+				else if (count == 0)text.setString("");
+				else text.setString(std::to_string(count));
+			}
+			else if (pole->GetCellStatus(i, j) == 1){//Уже открытая клеточка
+				Cell.setTextureRect(sf::IntRect(25, 0, 25, 25));
+				text.setFillColor(Color::Blue);
+				int count = pole->GetCellNumber(i, j);
+				if (count == 9)text.setFillColor(Color::Black);
+				else if (count == 0)text.setString("");
+				else text.setString(std::to_string(count));
+			}
+			else if (pole->GetCellStatus(i, j) == 2) {//Клеточка помечена как заминированная
+				Cell.setTextureRect(sf::IntRect(50, 0, 25, 25));
+			}
+			else if (pole->GetCellStatus(i, j) == 3) {//Мина взорвана
+				Cell.setTextureRect(sf::IntRect(12, 0, 25, 25));
+			}
 
-    for (unsigned int i = 0; i < pole->GetHeight(); i++) {
-        for (unsigned int j = 0; j < pole->GetWidth(); j++) {
-            text.setString("");
-            if (pole->GetCellStatus(i, j) == 0) {// Не открытая клеточка
-                NotOpenCellTexture.loadFromFile("Images/Kletka.png");
-                Cell.setTexture(NotOpenCellTexture);
-
-                text.setFillColor(Color::Blue);
-                text.setString(std::to_string(pole->GetCellNumber(i, j)));
-                if (pole->GetCellNumber(i, j) == 9)text.setFillColor(Color::Black);
-                if (pole->GetCellNumber(i, j) == 0)text.setString("");
-            }
-            else if (pole->GetCellStatus(i, j) == 1) {//Уже открытая клеточка
-                NotOpenCellTexture.loadFromFile("Images/Kletka.png");
-                Cell.setTexture(NotOpenCellTexture);
-                text.setFillColor(Color::Magenta);
-                text.setString(std::to_string(pole->GetCellNumber(i, j)));
-                if (pole->GetCellNumber(i, j) == 0)text.setString("");
-            }
-            else if (pole->GetCellStatus(i, j) == 2) {//Клеточка помечена как заминированная
-                NotOpenCellTexture.loadFromFile("Images/Kletka.png");
-                Cell.setTexture(NotOpenCellTexture);
-                text.setFillColor(Color::Red);
-                text.setString("1");
-            }
-            else if (pole->GetCellStatus(i, j) == 3) {//Мина взорвана
-                NotOpenCellTexture.loadFromFile("Images/Kletka.png");
-                Cell.setTexture(NotOpenCellTexture);
-                text.setFillColor(Color::Black);
-                text.setString("");
-            }
-
-            // Вычисление позиции клеточки для отрисовки
-            Vector2f position(j * Cell::size + 11.f, i * Cell::size + 52.f);
-            Vector2f textPosition(j * Cell::size + 15.f, i * Cell::size + 50.f);
-
-            text.setPosition(textPosition);
-            Cell.setPosition(position);
-            target.draw(Cell);
-            target.draw(text, states);
-        }
-    }
-}
-
-void Game::OpenCell(int x, int y) {
-    pole->Open(x, y);
-}
-
-int Game::GetLevel() {
-    return level;
-}
-
-Game::~Game() {
-    delete pole;
+			// Вычисление позиции клеточки для отрисовки
+			Vector2f position(j * Cell::size + 11.f, i * Cell::size + 52.f);
+			Vector2f textPosition(j * Cell::size + 15.f, i * Cell::size + 50.f);
+			
+			text.setPosition(textPosition);
+			Cell.setPosition(position);
+			target.draw(Cell);
+			target.draw(text, states);
+		}
+	}
 }
 
 void Game::SetSelfStatus(int x, int y) {
